66ff98841e6f9154ccfebe2de8b8f0ed37ac69d8
AnnJBarr <annbarr@hotmail.com>
2019-11-05 11:28:05 +0000

    passing dates sorted for Completed tasks. Overdue styling added

--- src/components/CompletedTasks.js
+++ src/components/CompletedTasks.js
@@ -16,7 +16,7 @@ class CompletedTasks extends React.Component {
                 <div className="card-body">
                     <div className="card-text">
                         {dateSortedCompleted.map(task => {
-                            return <TaskItem text={task.text} done={task.done} key={task.id} />
+                            return <TaskItem doneTaskFunc={this.doneTask} text={task.text} done={task.done} key={task.id} dateAdded={task.dateAdded} dateCompleted={task.dateCompleted} dueBy={task.dueBy} />
                         })}
                     </div>
                     <button id="task-button" className="btn"><FontAwesomeIcon icon={faChevronCircleDown} /></button>
