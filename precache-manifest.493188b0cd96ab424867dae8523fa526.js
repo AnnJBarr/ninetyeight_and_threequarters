700d7b4a662dcd97e18a0f77d025a3b1e01ebcbc
AnnJBarr <annbarr@hotmail.com>
2019-11-06 12:26:27 +0000

    Updates

---
+++ precache-manifest.493188b0cd96ab424867dae8523fa526.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217b67407a6b5445076e9c2f16ecf4f9",
+    "url": "/ninetyeight_and_threequarters/index.html"
+  },
+  {
+    "revision": "aae7888e1a46ef191086",
+    "url": "/ninetyeight_and_threequarters/static/css/2.78f95891.chunk.css"
+  },
+  {
+    "revision": "5505da88297fb7f554e0",
+    "url": "/ninetyeight_and_threequarters/static/css/main.b81ebfab.chunk.css"
+  },
+  {
+    "revision": "aae7888e1a46ef191086",
+    "url": "/ninetyeight_and_threequarters/static/js/2.1fa466df.chunk.js"
+  },
+  {
+    "revision": "5505da88297fb7f554e0",
+    "url": "/ninetyeight_and_threequarters/static/js/main.25f093d6.chunk.js"
+  },
+  {
+    "revision": "469fbad61625192f5997",
+    "url": "/ninetyeight_and_threequarters/static/js/runtime-main.fa508235.js"
+  }
+]);
